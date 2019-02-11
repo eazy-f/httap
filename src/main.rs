ec9cc8930638645b12e9a36d93948b5f9cbd9fee
Nikita Lebedev <eazy-f@yandex.ru>
2019-02-11 14:35:07 +0300

    Create remote suspended thread

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate winapi;
 
 #[cfg(windows)]
-use winapi::um::processthreadsapi::OpenProcess;
+use winapi::um::processthreadsapi::{
+    OpenProcess,
+    CreateRemoteThread
+};
 #[cfg(windows)]
 use winapi::um::winnt::{
     PROCESS_CREATE_THREAD,
@@ -9,6 +12,8 @@ use winapi::um::winnt::{
     MEM_COMMIT,
     PAGE_EXECUTE_READWRITE
 };
+#[cfg(windows)]
+use winapi::um::winbase::CREATE_SUSPENDED;
 #[cfg(windows)]
 use winapi::shared::ntdef::FALSE;
 #[cfg(windows)]
@@ -33,12 +38,29 @@ fn load(pid: u32){
     let process = unsafe {
         OpenProcess(PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION, FALSE as i32, pid)
     };
-    win_panic_handle(process, "Cannot open process");
+    win_panic_handle(process, "cannot open process");
     let alloc_size = 1024*1024;
     let allocation = unsafe {
         VirtualAllocEx(process, ptr::null_mut(), alloc_size, MEM_COMMIT, PAGE_EXECUTE_READWRITE)
     };
-    win_panic_handle(allocation, "Cannot allocate memory in the remote process");
+    win_panic_handle(allocation, "cannot allocate memory in the remote process");
+    let stack_size = 512 * 1024;
+    let parameter = ptr::null_mut();
+    let creation = CREATE_SUSPENDED;
+    let security = ptr::null_mut();
+    let thread_id = ptr::null_mut();
+    let start = unsafe {
+        let ptr = std::mem::transmute::<*mut winapi::ctypes::c_void, unsafe extern "system" fn(*mut winapi::ctypes::c_void) -> u32>(allocation);
+        Some(ptr)
+    };
+    let thread = unsafe {
+        CreateRemoteThread(
+            process, security, stack_size, start,
+            parameter, creation, thread_id
+        )
+    };
+    win_panic_handle(thread, "failed to create remote thread");
+    println!("thread id: {}", thread as u64);
 }
 
 #[cfg(not(windows))]
